056c5deb4c5885eed1296311aad2c928ea5c6dd7
Edmund <edmundgachanja@gmail.com>
2025-01-20 11:03:20 +0300

    Finishes the lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,23 @@
 // create your App component here
+import React,{ useEffect } from "react";
+import { useState } from "react";
+ 
+export default function App () {
+    const [ isData, setIsData] = useState([])
+    const [isLoaded, setIsLoaded] = useState(false)
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                setIsData(data.message)
+                setIsLoaded(true)
+            })
+    },[])
+    if (!isLoaded) {
+        return <p>Loading...</p>
+    }
+
+    return <img src={isData} alt="A Random Dog" />
+}
